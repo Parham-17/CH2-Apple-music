b3733657ea31dec8b89704216b4431737c62874e
Parham <parham.karbasi78@gmail.com>
2025-11-11 02:34:10 +0100

    Initial Commit

---
+++ AMHome/AMHomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AMHomeApp.swift
+//  AMHome
+//
+//  Created by Parham Kharbasi on 11/11/25.
+//
+
+import SwiftUI
+
+@main
+struct AMHomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
